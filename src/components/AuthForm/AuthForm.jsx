f36653cf0f046bd8de0cc74905de3902f318a2d1
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-02-24 20:34:13 -0800

    authentication response from supabase

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react"
 import { useForm } from "../../hooks/useForm"
 
-export default function AuthForm() {
+export default function AuthForm({ handleAuth }) {
     const { formState, handleFormChange, formError, setFormError } = useForm({ email: '', password: '' })
+    const [loading, setLoading] = useState(false)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { email, password } = formState;
+
+        try {
+            setFormError('');
+            if (!email || password.length < 8)
+            throw new Error('Please enter a valid e-mail and a password with minimum of eight characters');
+            setLoading(true);
+            await handleAuth(email, password)
+        } catch (error) {
+            setFormError(error.message)
+        } finally {
+            setLoading(false);
+        }
+
+    }
+
   return (
     <div>
-        <form className="flex flex-col">
+        <form onSubmit={handleSubmit} className="flex flex-col">
+            {formError && <p className="text-sm text-lime-600">{formError}</p>}
             <input className="m-5" type="email" placeholder="email" name="email" value={formState.email} onChange={handleFormChange} />
             <input className="m-5" type="password" placeholder="password" name="password" value={formState.password} onChange={handleFormChange} />
-            <button className="m-5 border-2 border-lime-600 rounded bg-amber-200" >Go!</button>
+            <button className="m-5 border-2 border-lime-600 rounded bg-amber-200" type="submit">Go!</button>
         </form>
     </div>
   )
--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,7 +1,22 @@
 import { Link } from "react-router-dom"
 import AuthForm from "../../components/AuthForm/AuthForm"
-
+import { signInUser, signUpUser } from '../../services/users';
 export default function Auth({ isSigningUp = false }) {
+
+const handleAuth = async (email, password) => {
+    try {
+        if (isSigningUp) {
+            await signUpUser(email, password)
+        } else {
+            const response = await signInUser(email, password)
+            console.log(response);
+
+        }
+     } catch (error) {
+        throw error;
+    }
+};
+
   return (
       <div
       className="
@@ -18,7 +33,7 @@ export default function Auth({ isSigningUp = false }) {
       "
       >
         <h2 className="mb-10">{isSigningUp ? 'Sign Up' : 'Sign In'} </h2>
-        <AuthForm label={isSigningUp ? 'Sign Up' : 'Sign In'}  />
+        <AuthForm handleAuth={handleAuth} label={isSigningUp ? 'Sign Up' : 'Sign In'}  />
         {isSigningUp ? (<Link className="text-xl text-rose-400 underline" to="/signin">I already have an account!</Link>) : (<Link className="text-xl text-rose-400 underline" to="/signup">I need to create an account!</Link>)}
       </div>
   )
