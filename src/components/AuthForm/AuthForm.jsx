328f78b75908bfa8182370f0bb8488083d844fdd
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-02-24 17:13:18 -0800

    imput fields update formState

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,9 +1,12 @@
+import { useForm } from "../../hooks/useForm"
+
 export default function AuthForm() {
+    const { formState, handleFormChange, formError, setFormError } = useForm({ email: '', password: '' })
   return (
     <div>
         <form className="flex flex-col">
-            <input className="m-5" type="email" placeholder="email" name="email" />
-            <input className="m-5" type="password" placeholder="password" name="password"  />
+            <input className="m-5" type="email" placeholder="email" name="email" value={formState.email} onChange={handleFormChange} />
+            <input className="m-5" type="password" placeholder="password" name="password" value={formState.password} onChange={handleFormChange} />
             <button className="m-5 border-2 border-lime-600 rounded bg-amber-200" >Go!</button>
         </form>
     </div>
