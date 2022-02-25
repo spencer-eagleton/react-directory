5966e2ceba7e651d6c1a3239d957a8cf9fb58781
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-02-25 10:09:38 -0800

    created usercontext with state

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+import { getUser } from "../services/users";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+    const currentUser = getUser();
+    const [user, setUser] = useState(currentUser ? { id: currentUser.id, email: currentUser.email} : {});
+
+    value = ({ user, setUser, currentUser })
+
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error('use useUser under UserProvider only');
+    }
+
+    return context;
+}
