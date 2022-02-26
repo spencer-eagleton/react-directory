71a90f66354666fe0359a9a726e1d55b8c579ab1
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-02-25 20:55:52 -0800

    header styles minus footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import styles from './App.module.css'; /* CSS Modules */
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Welcome from './views/Welcome/Welcome';
 import Header from './components/layout/Header';
-import Footer from './components/layout/Footer'
 import Auth from './views/Auth/Auth';
 import Profile from './views/Profile/Profile';
 import { UserProvider } from './context/UserContext';
@@ -32,7 +31,6 @@ export default function App() {
             <Welcome path="/" />
           </Route>
         </Switch>
-        <Footer />
       </div>
     </BrowserRouter>
     </UserProvider>
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,20 @@
 
 export default function Header() {
   return (
-    <div>Header</div>
+<nav class="flex items-center justify-between flex-wrap p-6">
+  <div class="flex items-center flex-no-shrink text-white mr-6">
+    <svg class="h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+    <span class="font-semibold text-xl tracking-tight">Employee Directory</span>
+  </div>
+  
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-sm lg:flex-grow">
+
+    </div>
+    <div>
+      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 mt-0">Sign Out</a>
+    </div>
+  </div>
+</nav>
   )
 }
